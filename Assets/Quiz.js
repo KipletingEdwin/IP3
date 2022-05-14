1ff9aa8a233ee7b0c696d912108d236da79a49a2
KipletingEdwin <kipletingedwin4@gmail.com>
2022-05-14 15:27:18 +0300

    will add a ReadMe file

--- Assets/Quiz.js
+++ Assets/Quiz.js
@@ -9,4 +9,53 @@ var correctAnswers={
 //capture data when users submit answers//
 let form=document.getElementById("form")
 var score=0;
-form.addEventListener("submit",function (event))
+form.addEventListener("submit",function (event)){
+    //alert("Go and submit")
+    Event.preventDefault();
+
+    var userAnswers=newFormData(form);
+    getData(userAnswers);
+}
+
+//calculating score after submission//
+function getData(userAnswers){
+    var question1 = userAnswers.get("question1");
+    var question2 = userAnswers.get("question2");
+    var question3 = userAnswers.get("question3");
+    var question4 = userAnswers.get("question4");
+    var question5 = userAnswers.get("question5");
+
+
+    if (question1 == correctAnswers["question1"]){
+    score +=20;
+    }
+    if (question2 == correctAnswers["question2"]){
+        score +=20;
+    }
+    if (question3 == correctAnswers["question3"]){
+        score +=20;
+    }
+    if (question4 == correctAnswers["question4"]){
+        score +=20;
+    }
+    if (question5 == correctAnswers["question5"]){
+      score +=20;
+    }
+
+    //results printed on screen//
+    let userScore = document.getElementsByClassName(".score");
+    // userScore.textContent = 'You Scored :'+ '' + score; 
+    document.write ("Your score is:  " + '' + score);
+
+    if(score >= 80) {
+        document.write("Congratulations, You've Excellently passed. ");
+      }else if
+        (score >= 50 && score < 80){
+        document.write("Good Work, You have Fairly passed. ");
+      }else {
+          document.write( "You have to retake the quiz");
+        }
+
+        var delayInMilliseconds=7000;
+        window.location.reload();
+    }delayInMilliseconds);
