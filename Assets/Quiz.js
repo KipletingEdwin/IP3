559a4c841a7fee8a000d2139f06318be7dd3e7b4
KipletingEdwin <kipletingedwin4@gmail.com>
2022-05-14 18:50:38 +0300

    prints out results

--- Assets/Quiz.js
+++ Assets/Quiz.js
@@ -1,56 +1,58 @@
-//storing correct values here//
-var correctAnswers={
-    question1 :"HTML"
-    question2 :"ECMAscript"
-    question3 :"Yes"
-    question4 :"Script"
-    question5 :"for"
-};
-//capture data when users submit answers//
-let form=document.getElementById("form")
 
-var score=0;
 
-form.addEventListener("submit",function (event)){
-    //alert("Go and submit")
+var correctAnswers = {
+    question1 : "1",
+    question2 : "Class",
+    question3 : "1995",
+    question4 : "script",
+    question5 : "For"
+  };
+   
+
+  let form = document.getElementById("form");
+  
+
+  var score = 0;
+  
+  form.addEventListener ("submit", function (event) { 
+    
+
     event.preventDefault();
-
-    var userAnswers=newFormData(form);
+    var userAnswers = new FormData(form);   
     getData(userAnswers);
-}
+  })
+  
+  
 
-//calculating score after submission//
-function getData(userAnswers){
+  function getData(userAnswers) {
     var question1 = userAnswers.get("question1");
     var question2 = userAnswers.get("question2");
     var question3 = userAnswers.get("question3");
     var question4 = userAnswers.get("question4");
     var question5 = userAnswers.get("question5");
-
-
+  
+     
     if (question1 == correctAnswers["question1"]){
-    score +=20;
-    }
-    if (question2 == correctAnswers["question2"]){
-        score +=20;
-    }
-    if (question3 == correctAnswers["question3"]){
-        score +=20;
-    }
-    if (question4 == correctAnswers["question4"]){
-        score +=20;
-    }
-    if (question5 == correctAnswers["question5"]){
-      score +=20;
-    }
-
-    //results printed on screen//
+      score +=20;  
+    } 
+    if (question2 == correctAnswers["question2"]) {
+      score += 20;  
+    } 
+    if (question3 == correctAnswers["question3"]) {
+      score += 20;  
+    } 
+    if (question4 == correctAnswers["question4"]) {
+      score += 20;  
+    } 
+    if (question5 == correctAnswers["question5"]) {
+      score += 20;  
+    } 
     
     let userScore = document.getElementsByClassName(".score");
-    // userScore.textContent = 'You Scored :'+ '' + score; 
-    document.write ("Your score is:  " + '' + score);
-
-    if(score >= 80) {
+       
+      document.write ("Your score is:  " + '' + score);
+  
+      if(score >= 80) {
         document.write("Congratulations, You've Excellently passed. ");
       }else if
         (score >= 50 && score < 80){
@@ -58,7 +60,9 @@ function getData(userAnswers){
       }else {
           document.write( "You have to retake the quiz");
         }
-
-        var delayInMilliseconds=7000;
+      var delayInMilliseconds = 7000; //7 seconds
+      setTimeout(function() {
+        //Clearing of form fields after some seconds
         window.location.reload();
-    }delayInMilliseconds);
+      }, delayInMilliseconds);
+  }
